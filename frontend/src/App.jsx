0ce81b529953c1c86941a860b62a9cd70d1836b4
Vishnu M <vishnuofficial004@gmail.com>
2026-05-08 02:28:38 +0530

    chore: configure vite dev server and clean up boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Distributed Order Tracking</h1>
+      <p>Project setup complete.</p>
+    </div>
+  );
+};
+
+export default App;
